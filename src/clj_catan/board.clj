4d62c26b848e958ab32b755a9baa82b8aa3090a6
Jared Langson <langson.jared@gmail.com>
2022-02-07 11:04:03 -0500

    Define which tiles are neighbors

--- src/clj_catan/board.clj
+++ src/clj_catan/board.clj
@@ -17,12 +17,32 @@
             11 2
             12 1})
 
-(def harbors {"bricK"   1
+(def harbors {"brick"   1
               "generic" 4
               "sheep"   1
               "ore"   1
               "wheat"   1
               "wood"    1})
+
+(def neighbors {1 [2 3 4 5 6 7]
+                2 [1 3 7 8 9 10]
+                3 [1 2 4 10 11 12]
+                4 [1 3 5 12 13 14]
+                5 [1 4 6 14 15 16]
+                6 [1 5 7 16 17 18]
+                7 [1 2 6 8 18 19]
+                8 [2 7 8 18]
+                9 [2 8 10]
+                10 [2 3 9 11]
+                11 [3 10 12]
+                12 [3 4 11 13]
+                13 [4 12 14]
+                14 [4 5 13 15]
+                15 [5 14 16]
+                16 [5 6 15 17]
+                17 [6 16 18]
+                18 [6 7 17 19]
+                19 [7 8 18]})
 
 (defn place
   [item-quantity locations output]
@@ -37,22 +57,15 @@
         (place (dissoc item-quantity item) new-locations (assoc output location item))
         (place (assoc item-quantity item new-quantity) new-locations (assoc output location item))))))
 
+(def place-6-8
+  "Seeds the board by placing the sixes and eights on non-adjacent tiles"
+  [])
+
 (defn setup-board []
   {:harbors   (place harbors (range 1 10) {})               ; sum of values of harbors is 9
    :rolls     (place rolls (range 1 19) {})
    :resources (place resources (range 1 20) {})}) ;higher range b/c desert
 
 (comment
-  (for [kv rolls]
-    (reduce + 0 (second kv)))
 
-  (for [kv rolls]
-    (let [values (second kv)]
-      (println (type values))
-      (->> values
-           vector
-           (apply +))))
-
-  (for [kv (into [] rolls)]
-    (apply + (second )))
   )
