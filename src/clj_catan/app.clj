d00f397c53e1f392721a6d71e6dd835854ceec34
Jared Langson <langson.jared@gmail.com>
2022-06-27 15:54:39 -0400

    Outline routes

--- src/clj_catan/app.clj
+++ src/clj_catan/app.clj
@@ -5,3 +5,20 @@
             [compojure.route :refer [not-found]]
             [clojure.data.json :as json]
             [clojure.string :as str]))
+
+
+(defn make-board [])
+
+(def home-page [])
+
+(def login [])
+
+(defroutes app
+  (GET "/make-board" [] make-board)
+  (GET "/" [] home-page)
+  (GET "/" [] login)
+  (not-found "404 not found!"))
+
+
+(defn -main [& args]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer/parseInt (first args))}))
