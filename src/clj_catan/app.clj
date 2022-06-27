1167a69442c8dd44029610c37439c83b8b4d0b81
Jared Langson <langson.jared@gmail.com>
2022-06-27 16:32:07 -0400

    Add pseudo-content to routes

--- src/clj_catan/app.clj
+++ src/clj_catan/app.clj
@@ -1,24 +1,32 @@
 (ns clj-catan.app
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap.reload]]
+            [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found]]
             [clojure.data.json :as json]
             [clojure.string :as str]))
 
 
-(defn make-board [])
+(def make-board
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "make the board"})
 
-(def home-page [])
+(def home-page
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello home-page"})
 
-(def login [])
+(def login
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello login"})
 
 (defroutes app
   (GET "/make-board" [] make-board)
   (GET "/" [] home-page)
-  (GET "/" [] login)
+  (GET "/login" [] login)
   (not-found "404 not found!"))
-
 
 (defn -main [& args]
   (jetty/run-jetty (wrap-reload #'app) {:port (Integer/parseInt (first args))}))
