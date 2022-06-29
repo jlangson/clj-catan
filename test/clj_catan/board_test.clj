88840172b3a50814e85fbfe550b44e936bb1f31c
Jared Langson <langson.jared@gmail.com>
2022-06-29 11:48:44 -0400

    Initialize board-test.clj

---
+++ test/clj_catan/board_test.clj
@@ -0,0 +1,6 @@
+(ns clj-catan.board-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as gen]))
+
