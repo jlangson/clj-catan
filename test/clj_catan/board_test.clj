90c2c367357f94eeeb998e07051f5cabf7a2dd12
Jared Langson <langson.jared@gmail.com>
2022-07-06 11:01:18 -0400

    Create function count-reversed-keys

--- test/clj_catan/board_test.clj
+++ test/clj_catan/board_test.clj
@@ -5,29 +5,8 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.generators :as gen]))
 
-(comment
-  ;generate the vector
-  (gen/sample (gen/vector (gen/vector gen/nat 2 2)))
-  (gen/sample (gen/vector gen/nat 2 2))
-  ;vector -> hashmap
-  (apply assoc {} (flatten (gen/sample (gen/vector gen/nat 2 2))))
-
-  (map reverse (into [] (:harbors (b/setup-board))))
-  (as-> (b/setup-board) b
-    (:harbors b)
-    (into [] b)
-    (map reverse b))
-
-  (->> (b/setup-board)
-    (:harbors)
-    (into [])
-    (map reverse))
-  )
-
 (defn count-reversed-keys
-  ;todo wrong description. look at the hashmaps and think more clearly.
-  ; it is about the count, not the sum
-  "Takes a map, reverses kv and changes values to the count of the former keys
+  "Takes a map, reverses kv and changes values to the frequencies of the former keys
   ex: {1 :a
        2 :a
        3 :b
@@ -36,8 +15,12 @@
        ->
        {:a 2
        :b 2
-       :c 1"
-  [hash-map])
+       :c 1}"
+  [hash-map]
+  (->> hash-map
+    (into [])
+    (map second)
+    (frequencies)))
 
 (defspec place-inverse-test 100
   (prop/for-all [board-vector (gen/vector (gen/vector gen/nat 2 2))]
@@ -46,3 +29,24 @@
          board (b/place board-map (b/count-locations board-map) {})]
      (= board-map false))))
 
+(comment
+  ;generate the vector
+  (gen/sample (gen/vector (gen/vector gen/nat 2 2)))
+  (gen/sample (gen/vector gen/nat 2 2))
+  ;vector -> hashmap
+  (apply assoc {} (flatten (gen/sample (gen/vector gen/nat 2 2))))
+
+  (count-reversed-keys (apply assoc {} (flatten (gen/sample (gen/vector gen/nat 2 2)))))
+
+  (frequencies (map second (into [] (:harbors (b/setup-board)))))
+  ;an outline of count-reversed keys
+  (->> (b/setup-board)
+    (:harbors)
+    (into [])
+    (map second)
+    (frequencies))
+
+  (count-reversed-keys (:harbors (b/setup-board)))
+  (count-reversed-keys (:rolls (b/setup-board)))
+  (count-reversed-keys (:resources (b/setup-board)))
+  )
