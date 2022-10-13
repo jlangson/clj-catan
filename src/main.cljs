5e5a070e184b416ef6d967c243bbf433f124701a
Jared Langson <langson.jared@gmail.com>
2022-10-13 11:17:38 -0400

    Add skeleton of reagent front end
    - Has bug. Does not display `simple-component`

---
+++ src/main.cljs
@@ -0,0 +1,27 @@
+(ns main
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+
+(defn simple-component []
+  [:div
+   [:p "I am a component"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+
+(defn mount! []
+  (rdom/render [app]
+                  (.getElementById js/document "app")))
+
+(defn main! []
+  (mount!))
+
+(defn reload! []
+  (mount!))
+
+;(defn ^:export start! []
+;  (rdom/render [simple-component] (js/document.getElementById "app")))
+
+;(defn reload! [])
