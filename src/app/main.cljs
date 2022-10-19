50105da69ac1a3f0335eaa30d268e0c47bfd6bde
Jared Langson <langson.jared@gmail.com>
2022-10-19 18:23:23 -0400

    Implement button that renders new board

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,19 +1,19 @@
 (ns app.main
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [front-end.board :as board]
-            ))
+            [front-end.board :as b]))
 
-(defn make-board-button []
-  [:div
-   [:button "Generate Board"]])
+(def board (r/atom (b/setup-board)))
 
 (defn app []
-  [:div
-   [:h1 "Your random board"]
-   [:br]
-   [:p (str (board/setup-board))]
-   (make-board-button)])
+  (let [state (r/atom true)]
+    [:div
+     [:h1 {:on-click #(js/alert "you clicked h1") }
+      "Your random board"]
+     [:br]
+     [:p (str @board)]
+     [:button {:on-click #(reset! board (b/setup-board))}
+      "Make a new board!"]]))
 
 (defn mount! []
   (rdom/render [app]
@@ -24,3 +24,13 @@
 
 (defn reload! []
   (mount!))
+
+
+(comment
+
+  (do
+    (reset! board (b/setup-board))
+    (print @board))
+
+
+  )
