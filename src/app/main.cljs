c4957efe5558f4ca5934f32e39c851b57ec47c61
Jared Langson <langson.jared@gmail.com>
2022-10-19 20:19:19 -0400

    Begin grid implementation
    - WIP
    - Looks bad b/c no CSS added from tailwind

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,16 +1,18 @@
 (ns app.main
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [front-end.board :as b]))
+            [front-end.board :as b]
+            [front-end.views :as views]))
 
 (def board (r/atom (b/setup-board)))
 
 (defn app []
   (let [state (r/atom true)]
     [:div
-     [:h1 {:on-click #(js/alert "you clicked h1") }
+     [:h1.text-blue-600 {:on-click #(js/alert "you clicked h1") }
       "Your random board"]
      [:br]
+     [views/grid]
      [:p (str @board)]
      [:button {:on-click #(reset! board (b/setup-board))}
       "Make a new board!"]]))
--- src/front_end/views.cljs
+++ src/front_end/views.cljs
@@ -1,7 +1,18 @@
 (ns front-end.views)
 
-[:canvas {:width 500
-          :height 500
-          :id "cv"
-          :style {:border "1px solid black" :display "block"}}]
-[:button {:on-click #(re-frame.core/dispatch [:draw-grid (.getElementById js/document "Draw grid")])}]
+(def colors {"ore" "black"
+             "sheep" "blue"                                 ;blue so it is not confused with wood. todo choose better colors
+             "wheat" "yellow"
+             "brick" "brown"
+             "wood" "green"})
+
+(defn grid []
+  [:h2 "this is a grid"])
+
+(comment
+  [:canvas {:width  500
+            :height 500
+            :id     "cv"
+            :style  {:border "1px solid black" :display "block"}}]
+  [:button {:on-click #(re-frame.core/dispatch [:draw-grid (.getElementById js/document "Draw grid")])}]
+  )
